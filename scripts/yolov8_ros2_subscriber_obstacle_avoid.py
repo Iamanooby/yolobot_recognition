5746b0a605fb8eb59405abf1bce2c66819bcb238
Chi Han <looichihan@gmail.com>
2023-08-04 10:25:53 +0800

    wip, cancel task not working. suspect only can cancel nav object tasks

--- scripts/yolov8_ros2_subscriber_obstacle_avoid.py
+++ scripts/yolov8_ros2_subscriber_obstacle_avoid.py
@@ -16,8 +16,7 @@ from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import LaserScan
 import math
 
-# from nav2_simple_commander.robot_navigator import BasicNavigator
-# nav = BasicNavigator()
+from nav2_simple_commander.robot_navigator import BasicNavigator
 from rclpy.action import ActionClient
 from geometry_msgs.msg import PoseStamped
 from nav2_msgs.action import NavigateToPose
@@ -123,87 +122,11 @@ class Yolo_subscriber(Node):
         self.z_ori = msg.pose.pose.orientation.z
         # print ("Message received: ", self.x_act, self.y_act,self.z_ori)
 
-    def movebase_client(self,goal_coordinates):
-        # rclpy.init()
-
-        # Create a ROS 2 node
-        node = rclpy.create_node('movebase_client')
-
-        # Create an action client called "navigate_to_pose" with action definition "NavigateToPose"
-        client = ActionClient(node, NavigateToPose, 'navigate_to_pose')
-
-        # Wait until the action server has started up and started listening for goals
-        if not client.wait_for_server(timeout_sec=5.0):
-            print("Action server not available!")
-            node.destroy_node()
-            rclpy.shutdown()
-            return
-
-        # Define a list of goal coordinates
-        # goal_coordinates = [
-        #     (2.0, 0.0, 0.0),  # x, y, theta for Goal 1
-        #     (1.0, -3.0, 0.0),  # x, y, theta for Goal 2
-        #     (1.0, 0.1, 0.5),  # x, y, theta for Goal 3
-        # ]
-        # goal_coordinates = [
-        #     (-1.0+2.0, -3.0+0.63, 0.0),  # x, y, theta for Goal 1
-        # ]
-
-
-        # Loop over the goal coordinates and send goals one by one
-        while True:
-            for goal_x, goal_y, goal_theta in goal_coordinates:
-                # Create a goal message with the PoseStamped message
-                goal_msg = NavigateToPose.Goal()
-
-                # Set the goal target pose
-                goal_msg.pose.header.frame_id = 'map'
-                goal_msg.pose.pose.position.x = goal_x
-                goal_msg.pose.pose.position.y = goal_y
-                goal_msg.pose.pose.orientation.z = goal_theta
-
-                # Send the goal to the action server
-                goal_handle_future = client.send_goal_async(goal_msg)
-
-                # Wait for the goal to complete
-                while rclpy.ok():
-                    rclpy.spin_once(node)
-                    if goal_handle_future.done():
-                        goal_handle = goal_handle_future.result()
-                        if goal_handle.accepted:
-                            print("Goal accepted.")
-                            result_future = goal_handle.get_result_async()
-                            rclpy.spin_until_future_complete(node, result_future)
-                            result = result_future.result().result
-                            if result!=False:
-                                print("Goal execution done!")
-                            else:
-                                print("Goal execution failed!")
-                            break
-                        else:
-                            print("Goal rejected.")
-                            break
-
-        # Clean up resources
-        node.destroy_node()
-        # rclpy.shutdown()
 
     def detect_obstacle(self):
         twist = Twist()
 
-        # if self.left_bound<self.image_width//2 and self.right_bound>self.image_width//2:#just do forward
-        #     obstacle_distance = self.scan_ranges[0]
-        #     angle = 0
-        # else:
-        #     forward_pixel_length = self.image_width/2/math.tan(31.1/180*math.pi)#half the fov angle
-        #     tolerance = 2#push closer to object
-        #     if self.left_bound>=self.image_width//2:#object on right half of camera
-        #         #forward is 0 degrees, then follow astc increment of angle where right is 270 degrees
-        #         angle = 360-round(math.atan(abs(self.left_bound-self.image_width//2)/forward_pixel_length)/math.pi*180)#obtain left most point angle
-        #         obstacle_distance = self.scan_ranges[angle-tolerance]
-        #     elif self.right_bound<=self.image_width//2:#object on right half of camera
-        #         angle = round(math.atan(abs(self.right_bound-self.image_width//2)/forward_pixel_length)/math.pi*180)#obtain left most point angle
-        #         obstacle_distance = self.scan_ranges[angle+tolerance]
+
 
         centre_of_picture = self.image_width/2
         centre_of_object = (self.right_bound+self.left_bound)//2
@@ -224,19 +147,10 @@ class Yolo_subscriber(Node):
 
         safety_distance = 1  # unit: m
         self.get_logger().info(f"Potted plant detected at {obstacle_distance}m away at {angle}deg.")
-        self.emergency_changed_goal = False
-        if obstacle_distance > safety_distance:
-            # twist.linear.x = self.linear_velocity
-            # twist.angular.z = self.angular_velocity
-            self.emergency_changed_goal = False
-        else:
-            if not self.emergency_changed_goal:
-                self.emergency_changed_goal = True
-                goal_coordinates = [[self.x_act+2.0,self.y_act+0.63,self.z_ori]]#give own cooridnates as new goal with adjustments
-
-                self.movebase_client(goal_coordinates)                
-
-
+        
+        if obstacle_distance < safety_distance:          
+            nav.cancelTask()
+            # nav.backup(backup_dist=1, backup_speed=0.26, time_allowance=10)
             twist.linear.x = 0.0
             twist.angular.z = 0.0
             self.cmd_vel_pub.publish(twist)    
@@ -245,6 +159,7 @@ class Yolo_subscriber(Node):
 
 if __name__ == '__main__':
     rclpy.init(args=None)
+    nav = BasicNavigator()
     yolo_subscriber = Yolo_subscriber()
 
     executor = rclpy.executors.MultiThreadedExecutor()
